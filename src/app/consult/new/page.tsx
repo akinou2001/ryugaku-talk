e4a573552e0b2a7ac8b80888035583c51082bade
akinou2001 <akinou2001@gmail.com>
2025-07-22 15:47:32 +0900

    add page.tsx

---
+++ src/app/consult/new/page.tsx
@@ -0,0 +1,5 @@
+// 例: src/app/consult/new/page.tsx
+export default function NewConsultPage() {
+    return <h2>相談投稿ページ（仮）</h2>;
+}
+  
---
+++ src/app/consult/page.tsx
@@ -0,0 +1,5 @@
+
+export default function ConsultListPage() {
+    return <h2>相談一覧ページ（仮）</h2>;
+}
+  
